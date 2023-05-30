a47792a742963c9d1f3e535b3f8b010165af396c
wabowen89 <wabowen89@gmail.com>
2023-05-30 15:36:33 -0700

    First Commit

---
+++ src/data.js
@@ -0,0 +1,31 @@
+export const projects = [
+  {
+    title: "React Reserve",
+    subtitle: "MERN Stack",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-1.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "React Tracks",
+    subtitle: "React and Python",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-2.gif",
+    link: "https://reactbootcamp.com",
+  },
+
+];
+
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "CSS",
+  "HTML",
+  "Ruby",
+  "Ruby on Rails",
+  "Java",
+  "PostgreSQL"
+];
