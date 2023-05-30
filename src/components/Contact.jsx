3f8af47a6cf3d86bdefeee85118946268d1a696c
wabowen89 <wabowen89@gmail.com>
2023-05-30 16:57:32 -0700

    Updated contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -29,7 +29,7 @@ export default function Contact() {
 
   return (
     <section id="contact" className="relative">
-      <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
+      <div className="container px-5 py-10 mx-auto flex justify-center items-center">
         <form
           netlify
           name="contact"
