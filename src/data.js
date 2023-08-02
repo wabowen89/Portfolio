e35740deb1c3d32526d5e426a926c4461c850b5c
wabowen89 <wabowen89@gmail.com>
2023-08-01 20:34:33 -0700

    added Tiki Treasure Hunt to projects object for future implementation and updated HoneyHome information

--- src/data.js
+++ src/data.js
@@ -1,12 +1,20 @@
 export const projects = [
   {
     title: "HoneyHome",
-    subtitle: "MERN Stack",
+    subtitle: "LEARN Capstone Project",
     description:
       "This is my full stack project completed with LEARN academy. I worked with four teammates to create this application. We utilized JS, React, Ruby, Ruby on Rails, and PostgreSQL",
     image: "./gifs/HoneyHome.gif",
     link: "https://github.com/orgs/Team-WHAK/repositories",
   },
+  // {
+  //   title: "Tiki Treasure Hunt",
+  //   subtitle: "LEARN Jumpstart Project",
+  //   description:
+  //     "This is the final project from LEARN academy's Jumpstart Weekend course. I worked on this project with one other teammate. It utilizes HTML, CSS, and Javascript.",
+  //   image: "./gifs/HoneyHome.gif",
+  //   link: "https://wabowen89.github.io/wabowen.github.io/",
+  // },
 
 
 ];
