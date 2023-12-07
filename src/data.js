8ed0cd1d8eed6a2936c32e06a121e8c6b0224dc9
wabowen89 <wabowen89@gmail.com>
2023-12-07 10:56:46 -0800

    updated project object to contain embedded proImages object. populated proImages object with temp picture references for testings

--- src/data.js
+++ src/data.js
@@ -6,6 +6,20 @@ export const projects = [
       "This project is a future gift for my nephew. I designed a 3D model of my father's 1942 Jeep Willy in Fusion 360. I am currently in the process of printing the body and attachments to the body. The vehicle is controlled utilizing two Arduino Uno R3's and an NRF24L antenna for radio communications. The Arduino's are programmed using the Arduino IDE. They are also controlling the rear motor and steering servo. Features to come: Speaker for Jeep sounds and LED headlights",
     image: "./gifs/HoneyHome.gif",
     link: "https://github.com/orgs/Jeep-Willy/repositories",
+    proImages: [
+      {
+        src: "./gifs/HoneyHome.gif",
+        alt: "temp",
+      },
+      {
+        src: "./gifs/HoneyHome.gif",
+        alt: "temp",
+      },
+      {
+        src: "./gifs/HoneyHome.gif",
+        alt: "temp",
+      },
+    ],
   },
   {
     title: "bLinkup Sample Application",
@@ -13,6 +27,20 @@ export const projects = [
     description: "temp text",
     image: "./gifs/HoneyHome.gif",
     link: "https://github.com/blinkupsdk/bLinkupKotlinSample",
+    proImages: [
+      {
+        src: "./gifs/HoneyHome.gif",
+        alt: "temp",
+      },
+      {
+        src: "./gifs/HoneyHome.gif",
+        alt: "temp",
+      },
+      {
+        src: "./gifs/HoneyHome.gif",
+        alt: "temp",
+      },
+    ],
   },
   {
     title: "HoneyHome",
@@ -21,6 +49,20 @@ export const projects = [
       "This is my full stack project completed with LEARN academy. I worked with four teammates to create this application. We utilized JS, React, Ruby, Ruby on Rails, and PostgreSQL along with Devise and JWT Authentication.",
     image: "./gifs/HoneyHome.gif",
     link: "https://github.com/orgs/Team-WHAK/repositories",
+    proImages: [
+      {
+        src: "./gifs/HoneyHome.gif",
+        alt: "temp",
+      },
+      {
+        src: "./gifs/HoneyHome.gif",
+        alt: "temp",
+      },
+      {
+        src: "./gifs/HoneyHome.gif",
+        alt: "temp",
+      },
+    ],
   },
 ];
 
