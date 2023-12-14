4152816a8c3799d58b83c2b579b22d788b2b658d
wabowen89 <wabowen89@gmail.com>
2023-12-14 09:56:40 -0800

    added additional project fields, css containers, and conditional rendering

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -5,32 +5,63 @@ export default function Modal({ project, handleCloseModal }) {
   return (
     <div className="modal-container">
       <div className="modal-overlay"></div>
-      <div className="modal">
-        <div className="modal-content">
-          <h1 className="title">{project.subtitle}</h1>
-          <p>{project.description}</p>
-          <h2 className="stack">Tech Stack</h2>
-          <ul>
-            {project.stack.map((item, index) => {
-              const stackItem = <li key={index}>{item}</li>;
-              return stackItem;
-            })}
-          </ul>
-          <div className="modal-images">
-            {project.proImages.map((image) => {
-              const imageElement = (
-                <img key={image} src={image.src} alt={image.alt} />
-              );
-              return imageElement;
-            })}
+      <div className="modal-content">
+        <h1 className="title">{project.subtitle}</h1>
+        <div className="content-wrapper">
+          <div className="description">
+            <h2 className="description-title">Description:</h2>
+            <p className="description-contents">{project.description}</p>
           </div>
-          {/* <button className="close-modal-button" onClick={handleCloseModal}>
+          <div className="tech-stack">
+            <h2 className="stack">Tech Stack:</h2>
+            <ul>
+              {project.stack.map((item, index) => {
+                const stackItem = <li key={index}>{item}</li>;
+                return stackItem;
+              })}
+            </ul>
+          </div>
+        </div>
+        <div className="links">
+          {(project.website || project.appStore) ? (
+            <h2>Links</h2>
+              ): (
+                <h2>Link</h2>
+          )}
+          <h3>
+            Github: <a href={project.github}>{project.github}</a>
+          </h3>
+          {project.website && (
+            <h3>
+              Website:{" "}
+              <a href={project.website} target="_blank">
+                {project.website}
+              </a>
+            </h3>
+          )}
+          {project.appStore && (
+            <h3>
+              App Store:{" "}
+              <a href={project.appStore} target="_blank">
+                {project.appStore}
+              </a>
+            </h3>
+          )}
+        </div>
+        <div className="modal-images">
+          {project.proImages.map((image) => {
+            const imageElement = (
+              <img key={image} src={image.src} alt={image.alt} />
+            );
+            return imageElement;
+          })}
+        </div>
+        {/* <button className="close-modal-button" onClick={handleCloseModal}>
             Close
           </button> */}
-        </div>
-        <div className="close-modal-icon" onClick={handleCloseModal}>
-          X
-        </div>
+      </div>
+      <div className="close-modal-icon" onClick={handleCloseModal}>
+        X
       </div>
     </div>
   );
