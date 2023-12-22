8b422b0e984af36f14336a08364773204b18c3f0
wabowen89 <wabowen89@gmail.com>
2023-12-21 16:55:22 -0800

    updated project object fields. added new data for bLinkup project

--- src/projects.js
+++ src/projects.js
@@ -22,9 +22,12 @@ export const projects = [
       "Add speaker and code to play jeep noises",
       "Add headlights and code to turn them on",
     ],
-    github: "https://github.com/orgs/Jeep-Willy/repositories",
-    website: "",
-    appStore: "",
+    links: [
+      {
+        title: "Github",
+        address: "https://github.com/orgs/Jeep-Willy/repositories",
+      },
+    ],
     proImages: [
       {
         src: "./assets/jeep/jeep-full.png",
@@ -101,26 +104,80 @@ export const projects = [
   {
     title: "bLinkup Sample Application",
     subtitle: "Demonstration Application for bLinkup SDK",
-    description: "temp text",
-    src: "./gifs/HoneyHome.gif",
+    description:
+      'This mobile application, specially designed for Android devices, unveils the capabilities of the bLinkup SDK, offering developers a captivating experience. It seamlessly blends various features to enhance social connections and event engagement. Users can effortlessly befriend each other within the app, receive timely notifications when their bLinkup friends arrive at events, and navigate interactive venue maps dotted with clickable "bLinkpoints"—strategically placed by event owners—to effortlessly coordinate meet-ups by sharing the location information directly through apps on their phones. Geofencing technology eliminates the need for manual check-ins, automatically notifying users when they enter designated venues. The app also integrates with phone contacts, allowing users to discover bLinkup users among their friends and send friend requests seamlessly. For developers, the app offers a manual check-in/out functionality, fostering convenient testing and debugging of events. This app is a powerful showcase of the bLinkup SDK\'s potential, revolutionizing user experiences with its suite of comprehensive and engaging features, crafted collaboratively by me and two other developers.',
+    src: "./assets/blinkup/login.jpg",
     alt: "bLinkup Sample App Login Screen",
     stack: ["Kotlin", "bLinkup SDK", "Android Studio"],
     proImages: [
       {
-        src: "./gifs/HoneyHome.gif",
-        alt: "temp",
+        src: "./assets/blinkup/login.jpg",
+        alt: "login-screen",
+        descr: "Login Screen",
       },
       {
-        src: "./gifs/HoneyHome.gif",
-        alt: "temp",
+        src: "./assets/blinkup/sms.jpg",
+        alt: "temp-passcode-screen",
+        descr: "Temporary Passcode Screen",
       },
       {
-        src: "./gifs/HoneyHome.gif",
-        alt: "temp",
+        src: "./assets/blinkup/friends-tab.jpg",
+        alt: "friends-tab",
+        descr: "Friends Tab",
+      },
+      {
+        src: "./assets/blinkup/phone-contacts.jpg",
+        alt: "phone-contacts-screen",
+        descr: "Match Phone Contacts Window",
+      },
+      {
+        src: "./assets/blinkup/pending-request.jpg",
+        alt: "pending-request-screen",
+        descr: "Pending Friend Request Window",
+      },
+      {
+        src: "./assets/blinkup/blocked-users.jpg",
+        alt: "blocked-users-screen",
+        descr: "Blocked Users Window",
+      },
+      {
+        src: "./assets/blinkup/presence-tab.jpg",
+        alt: "presence-tab",
+        descr: "Presence Tab",
+      },
+      {
+        src: "./assets/blinkup/dev-details.jpg",
+        alt: "dev-details-screen",
+        descr: "Developer Details Window",
+      },
+      {
+        src: "./assets/blinkup/map-tab.jpg",
+        alt: "map-tab",
+        descr: "Map Tab with List of Events",
+      },
+      {
+        src: "./assets/blinkup/map-display.jpg",
+        alt: "map-display",
+        descr: "Interactive Map Display with bLinkPoints",
+      },
+      {
+        src: "./assets/blinkup/blinkpoint-window.jpg",
+        alt: "blinkpoint-window",
+        descr: "Customizable Window for bLinkPoint",
+      },
+      {
+        src: "./assets/blinkup/settings-tab.jpg",
+        alt: "settings-tab",
+        descr: "Setting Tab",
       },
     ],
-    github: "https://github.com/blinkupsdk/bLinkupKotlinSample",
-    site: "",
+    features: ["Webhook notifications", "Google Play Store Link"],
+    links: [
+      {
+        title: "Github",
+        address: "https://github.com/blinkupsdk/bLinkupKotlinSample",
+      },
+    ],
   },
   {
     title: "HoneyHome",
@@ -153,7 +210,15 @@ export const projects = [
         alt: "temp",
       },
     ],
-    github: "https://github.com/orgs/Team-WHAK/repositories",
-    site: "",
+    links: [
+      {
+        title: "Github",
+        address: "https://github.com/orgs/Team-WHAK/repositories",
+      },
+      {
+        title: "Site",
+        address: "",
+      },
+    ],
   },
 ];
